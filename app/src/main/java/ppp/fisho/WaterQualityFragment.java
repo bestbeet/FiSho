1c035e4f530afb60c2a05939761eb270b6e3062c
Nattawat Thong-aram <bestbeet2538@gmail.com>
2017-09-10 03:13:28 +0700

    second comnit

--- app/src/main/java/ppp/fisho/WaterQualityFragment.java
+++ app/src/main/java/ppp/fisho/WaterQualityFragment.java
@@ -3,7 +3,9 @@ package ppp.fisho;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -20,6 +22,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
+import java.security.cert.Extension;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -31,19 +35,17 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class WaterQualityFragment extends Fragment {
 
-        public DatabaseReference myRef1,myRef2;
-        private TextView mFirebaseTextView1,mFirebaseTextView2;
-        private int notification_id = 001;
+    public DatabaseReference myRef1, myRef2;
+    private TextView mFirebaseTextView1, mFirebaseTextView2;
+        /*private int notification_id = 001;
         private NotificationCompat.Builder Builder;
-        private NotificationManager mNotifyMgr;
+        private NotificationManager mNotifyMgr;*/
+
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.waterquality_layout, container, false);
-
-
-
 
         mFirebaseTextView1 = (TextView) view.findViewById(R.id.WtemptextView);
         mFirebaseTextView2 = (TextView) view.findViewById(R.id.pHtextView);
@@ -61,32 +63,15 @@ public class WaterQualityFragment extends Fragment {
         myRef1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Map map = (Map)dataSnapshot.getValue();
+                Map map = (Map) dataSnapshot.getValue();
                 String value = String.valueOf(map.get("WaterTemp"));
                 mFirebaseTextView1.setText("Temperature : " + value + " C°");
 
-                if ( Float.parseFloat(value) >=  32 ){
-                    //
-                    Intent resultIntent = new Intent(getActivity(), MainActivity.class);
-                    PendingIntent resultPendingIntent = PendingIntent.getActivity(getActivity(), 0, resultIntent, 0);
-                    // แจ้งเตือนแบบขึ้นให้เห็น
-                    Builder = new NotificationCompat.Builder(getActivity())
-                            .setSmallIcon(R.drawable.icon_small)
-                            .setContentTitle("FiSho")
-                            .setContentText("High Temperature " + value)
-                            .setAutoCancel(true)
-                            .setContentIntent(resultPendingIntent)
-                            .setVibrate(new long[] {Notification.DEFAULT_VIBRATE})
-                            .setPriority(Notification.PRIORITY_MAX);
+                if (Float.parseFloat(value) <= 32) {
+                    getActivity().startService(new Intent(getActivity(), Notification_Fertilization.class));
+                } else {
 
-// Gets an instance of the NotificationManager service
-                    mNotifyMgr = (NotificationManager) getActivity().getSystemService(NOTIFICATION_SERVICE);
-// ตัวแสดงผล
-                    mNotifyMgr.notify(notification_id, Builder.build());
-
-                }
-                else {
-                    mFirebaseTextView1.setText("Temperature : " + value + " C°");
+                    getActivity().stopService(new Intent(getActivity(), Notification_Fertilization.class));
                 }
 
 
@@ -100,7 +85,7 @@ public class WaterQualityFragment extends Fragment {
         myRef2.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Map map = (Map)dataSnapshot.getValue();
+                Map map = (Map) dataSnapshot.getValue();
                 String value = String.valueOf(map.get("pH"));
                 mFirebaseTextView2.setText("pH : " + value);
 
@@ -111,7 +96,72 @@ public class WaterQualityFragment extends Fragment {
 
             }
         });
-        return  view;
+        return view;
         //return inflater.inflate(R.layout.waterquality_layout,null);
+    }
+}
+
+class Notification_Fertilization extends Service {
+
+    public DatabaseReference myRef1, myRef2;
+    private TextView mFirebaseTextView1, mFirebaseTextView2;
+    private int notification_id = 001;
+    private NotificationCompat.Builder Builder;
+    private NotificationManager mNotifyMgr;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef1 = database.getReference();
+        myRef1.keepSynced(true);
+        myRef1.orderByValue().limitToLast(1);
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+        Builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon_small)
+                .setContentTitle("FiSho")
+                .setContentText("High Temperature")
+                .setAutoCancel(true)
+                .setContentIntent(resultPendingIntent)
+                .setVibrate(new long[]{Notification.DEFAULT_VIBRATE})
+                .setPriority(Notification.PRIORITY_MAX);
+
+// Gets an instance of the NotificationManager service
+        mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String value = String.valueOf(map.get("WaterTemp"));
+                //String notification = dataSnapshot.child("Notification").getValue(String.class);
+                if (Float.parseFloat(value) >= 32)
+                    mNotifyMgr.notify(notification_id, Builder.build());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //we have some options for service
+        //start sticky means service will be explicity started and stopped
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //stopping the player when service is destroyed
     }
 }
