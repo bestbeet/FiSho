5b042842a5ccead767c601360fb9ba9791741c6d
Nattawat Thong-aram <bestbeet2538@gmail.com>
2017-11-07 02:27:27 +0700

    FoodLevel Update

--- app/src/main/java/ppp/fisho/FeedTimeFragment.java
+++ app/src/main/java/ppp/fisho/FeedTimeFragment.java
@@ -1,28 +1,144 @@
 package ppp.fisho;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import ppp.fisho.Notifications.Notification_AFood;
+import ppp.fisho.Service.AlarmReceiver;
 
 /**
  * Created by best on 5/4/2560.
  */
 
 public class FeedTimeFragment extends Fragment {
+    private TimePicker timefood;
+    private ToggleButton tb;
+    private Button mor,af,eve;
+    private EditText fm;
+    private DatabaseReference time;
+    AlarmManager alarmManager;
+    private PendingIntent pending_intent;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_feed_time,container,false);
         getActivity().setTitle("Feed Time");
-
-
-
-
+        timefood = (TimePicker)view.findViewById(R.id.TimeFood);
+        tb = (ToggleButton) view.findViewById(R.id.set1);
+        //mor = (Button) view.findViewById(R.id.morning);
+        af = (Button) view.findViewById(R.id.afternoon);
+        eve = (Button) view.findViewById(R.id.evening);
+        fm = (EditText) view.findViewById(R.id.minute);
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        time = database.getReference("FeedSet");
+        time.keepSynced(true);
+        time.orderByValue().limitToLast(1);
+
+        final Intent myIntent = new Intent(getActivity(), Notification_AFood.class);
+        alarmManager = (AlarmManager) getActivity().getSystemService(getActivity().ALARM_SERVICE);
+        final Calendar calendar = Calendar.getInstance();
+        timefood.setIs24HourView(true);
+
+
+        /*time.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String value, value1;
+                value = String.valueOf(map.get("Alert"));
+                value1 = String.valueOf(map.get("Notification"));
+                if (value.equals("Enable"))
+                    tv.setText("Set time: "  + String.format("%02d:%02d", timePicker.getHour(), timePicker.getMinute()));
+                if (value.equals("Disable"))
+                    tv.setText("System : Disable");
+                if (value.equals("Disable") && value1.equals("Enable")) {
+                    alarmManager.cancel(pending_intent);
+                    getActivity().stopService(new Intent(getActivity(), Notification_AFertilization.class));
+                    tv.setText("System : Disable");
+                } else ;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                time.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Map<String, Object> value = new HashMap<String, Object>();
+
+                        if (tb.isChecked()) {
+
+                            calendar.add(Calendar.SECOND, 3);
+                            final int hour = timefood.getCurrentHour();
+                            final int minute = timefood.getCurrentMinute();
+                            Log.e("MyActivity", "In the receiver with " + hour + " and " + minute);
+                            calendar.set(Calendar.HOUR_OF_DAY, timefood.getCurrentHour());
+                            calendar.set(Calendar.MINUTE, timefood.getCurrentMinute());
+                            myIntent.putExtra("extra", "yes");
+                            pending_intent = PendingIntent.getService(getActivity(), 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+                            if (fm.getText().toString().equals("")) {
+                                fm.setError("Please enter minute");
+                                value.put("Set1", "Disable");
+                            } else {
+                                value.put("Set1", hour);
+                                value.put("Secret", minute);
+                                value.put("DelaySet1", Integer.parseInt(fm.getText().toString()));
+                                time.updateChildren(value);
+                                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
+                            }
+                        }
+                        else{
+                            alarmManager.cancel(pending_intent);
+                            value.put("DelaySet1", "0");
+                            value.put("Set1", "Disable");
+                            value.put("Secret", "0");
+                            time.updateChildren(value);
+                        }
+
+                    }
+
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+
 
         return view;
     }
---
+++ app/src/main/java/ppp/fisho/Notifications/Notification_AFood.java
@@ -0,0 +1,109 @@
+package ppp.fisho.Notifications;
+
+/**
+ * Created by best on 7/11/2560.
+ */
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ppp.fisho.MainActivity;
+import ppp.fisho.R;
+
+public class Notification_AFood extends Service {
+    private DatabaseReference myRef;
+    private int notification_id;
+    private NotificationCompat.Builder builder;
+    private NotificationManager notificationManager;
+    private String secret;
+
+    private MediaPlayer player;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e("MyActivity", "In the FirstPlant service");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("AutoFertilization");
+        myRef.keepSynced(true);
+        myRef.orderByValue().limitToLast(1);
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        builder = new NotificationCompat.Builder(this);
+
+        notification_id = (int) System.currentTimeMillis();
+
+        Intent notification_intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notification_intent, 0);
+        builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon_small)
+                .setContentTitle("Auto Fertilization")
+                .setContentText("System : Success")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setVibrate(new long[]{Notification.DEFAULT_VIBRATE})
+                .setPriority(Notification.PRIORITY_MAX);
+
+
+        Map<String, Object> sec = new HashMap<String, Object>();
+        sec.put("Secret", "0");
+        myRef.updateChildren(sec);
+
+        //notificationManager.notify(notification_id, builder.build());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Object> value = new HashMap<String, Object>();
+                String notification = dataSnapshot.child("Notification").getValue(String.class);
+                String alert = dataSnapshot.child("Alert").getValue(String.class);
+                secret = dataSnapshot.child("Secret").getValue(String.class);
+                if (alert.equals("Enable") && secret.equals("0")) {
+                    value.put("Set1", "Enable");
+                    myRef.updateChildren(value);
+
+                }
+                if (notification.equals("Enable")) {
+                    notificationManager.notify(notification_id, builder.build());
+                    value.put("Secret", "1");
+                    myRef.updateChildren(value);
+                    //System.exit(0);
+                } else ;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //stopping the player when service is destroyed
+    }
+
+}
